05c77a0aaccac71f5d01fd7cabd4977cc513a24a
Ugyen Jigten <ujigten@gmail.com>
2020-12-12 15:12:36 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+function About() {
+  return (
+    <section class="about container">
+      <h2>Ugyen Jigten Dorji</h2>
+      <p>
+        Soufflé sesame snaps dragée. Cotton candy bear claw jelly sweet roll jelly beans.
+        Liquorice danish candy canes lollipop candy biscuit gingerbread cake.
+        Sugar plum topping marshmallow dessert pudding donut croissant.
+        </p>
+    </section>
+  )
+}
+
+function Contact() {
+  return (
+    <div>Contact</div>
+  )
+}
+
+function Work() {
+  return (
+    <div>Work</div>
+  )
+}
 
 function App() {
+
+  useEffect(() => { document.body.style.backgroundColor = "#f5f5f5" }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <About></About>
+      <Work></Work>
+      <Contact></Contact>
     </div>
   );
 }
